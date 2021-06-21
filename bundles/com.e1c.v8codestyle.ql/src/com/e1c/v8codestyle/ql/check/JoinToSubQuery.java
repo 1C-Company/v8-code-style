e2d0071c94be91089b6cee96529ad5ea2213e097
Dmitriy Marmyshev <dmar@1c.ru>
2021-06-21 21:48:02 +0300

    #1 Перенос проверок запроса (#10)

---
+++ bundles/com.e1c.v8codestyle.ql/src/com/e1c/v8codestyle/ql/check/JoinToSubQuery.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (C) 2021, 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial API and implementation
+ *******************************************************************************/
+package com.e1c.v8codestyle.ql.check;
+
+import static com._1c.g5.v8.dt.ql.model.QlPackage.Literals.QUERY_SCHEMA_QUERY_SOURCE_JOIN__SOURCE;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+
+import com._1c.g5.v8.dt.ql.model.QuerySchemaNestedQuery;
+import com._1c.g5.v8.dt.ql.model.QuerySchemaQuerySourceJoin;
+import com.e1c.g5.v8.dt.check.CheckComplexity;
+import com.e1c.g5.v8.dt.check.ICheckParameters;
+import com.e1c.g5.v8.dt.check.settings.IssueSeverity;
+import com.e1c.g5.v8.dt.check.settings.IssueType;
+import com.e1c.g5.v8.dt.ql.check.QlBasicDelegateCheck;
+
+/**
+ * The check of QL query that join with sub query, which lead to performance decrease.
+ *
+ * @author Dmitriy Marmyshev
+ */
+public class JoinToSubQuery
+    extends QlBasicDelegateCheck
+{
+
+    private static final String CHECK_ID = "ql-join-to-sub-query"; //$NON-NLS-1$
+
+    @Override
+    public String getCheckId()
+    {
+        return CHECK_ID;
+    }
+
+    @Override
+    protected void configureCheck(CheckConfigurer builder)
+    {
+        builder.title(Messages.JoinToSubQuery_title)
+            .description(Messages.JoinToSubQuery_description)
+            .complexity(CheckComplexity.NORMAL)
+            .severity(IssueSeverity.MAJOR)
+            .issueType(IssueType.PERFORMANCE)
+            .delegate(QuerySchemaQuerySourceJoin.class);
+    }
+
+    @Override
+    protected void checkQlObject(EObject object, QueryOwner owner, IQlResultAcceptor resultAceptor,
+        ICheckParameters parameters, IProgressMonitor monitor)
+    {
+        QuerySchemaQuerySourceJoin join = (QuerySchemaQuerySourceJoin)object;
+
+        if (join.getSource() != null && join.getSource().getSource() instanceof QuerySchemaNestedQuery)
+        {
+            resultAceptor.addIssue(Messages.JoinToSubQuery_Query_join_to_sub_query_not_allowed, object,
+                QUERY_SCHEMA_QUERY_SOURCE_JOIN__SOURCE);
+        }
+    }
+
+}
---
+++ bundles/com.e1c.v8codestyle.ql/src/com/e1c/v8codestyle/ql/check/Messages.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (C) 2021, 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial API and implementation
+ *******************************************************************************/
+/**
+ *
+ */
+package com.e1c.v8codestyle.ql.check;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * @author Dmitriy Marmyshev
+ *
+ */
+final class Messages
+    extends NLS
+{
+    private static final String BUNDLE_NAME = "com.e1c.v8codestyle.ql.check.messages"; //$NON-NLS-1$
+    public static String CamelCaseStringLiteral_description;
+    public static String CamelCaseStringLiteral_Regular_expression_to_skip_literal_content;
+    public static String CamelCaseStringLiteral_String_literal_contains_non_CamelCase_symbols__0;
+    public static String CamelCaseStringLiteral_title;
+    public static String CastToMaxNumber_description;
+    public static String CastToMaxNumber_Maximum_cast_number_length;
+    public static String CastToMaxNumber_Maximum_cast_number_precision_or_N_to_skip_check;
+    public static String CastToMaxNumber_Query_cast_to_number_with_lenth__0__and_max_allowed__1;
+    public static String CastToMaxNumber_Query_cast_to_number_with_precision__0__and_max_allowed__1;
+    public static String CastToMaxNumber_title;
+    public static String JoinToSubQuery_description;
+    public static String JoinToSubQuery_Query_join_to_sub_query_not_allowed;
+    public static String JoinToSubQuery_title;
+    static
+    {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+    private Messages()
+    {
+    }
+}
