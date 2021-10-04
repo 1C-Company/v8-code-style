f54f40874baa35264eb2cd3c144c01e84b1b08e9
Dmitriy Marmyshev <dmar@1c.ru>
2021-10-04 08:32:49 +0300

    #824 Автоматическое создание структуры модуля

--- bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/IModuleStructureProvider.java
+++ bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/IModuleStructureProvider.java
@@ -31,10 +31,10 @@ public interface IModuleStructureProvider
 {
 
     /** The key for preferences store the state of the creating module structure. */
-    static final String PREF_KEY_CREATE_STRUCTURE = "createModuleStructure"; //$NON-NLS-1$
+    final String PREF_KEY_CREATE_STRUCTURE = "createModuleStructure"; //$NON-NLS-1$
 
     /** The default value of creating module structure. */
-    static final boolean PREF_DEFAULT_CREATE_STRUCTURE = true;
+    final boolean PREF_DEFAULT_CREATE_STRUCTURE = true;
 
     /**
      * Can create module structure template for the project. This checks project or default settings.
