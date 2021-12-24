55727a9a2b03f9b81558625ee61ed75642c4c2d5
Dmitriy Marmyshev <dmar@1c.ru>
2021-12-24 22:30:26 +0200

    #887 Функциональные опции

--- bundles/com.e1c.v8codestyle.autosort/src/com/e1c/v8codestyle/internal/autosort/AutoSortProjectOptionProvider.java
+++ bundles/com.e1c.v8codestyle.autosort/src/com/e1c/v8codestyle/internal/autosort/AutoSortProjectOptionProvider.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (C) 2021, 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial API and implementation
+ *******************************************************************************/
 package com.e1c.v8codestyle.internal.autosort;
 
 import org.eclipse.core.resources.IProject;
