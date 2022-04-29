51652c93194a500b5c57337b9c03bc8e6f3de306
Olga Bozhko <olga.bozhko@xored.com>
2022-04-29 22:21:41 +0700

    #504 Add copyright

--- tests/com.e1c.v8codestyle.form.itests/src/com/e1c/v8codestyle/form/check/itests/FormListRefUserVisibilityEnabledCheckTest.java
+++ tests/com.e1c.v8codestyle.form.itests/src/com/e1c/v8codestyle/form/check/itests/FormListRefUserVisibilityEnabledCheckTest.java
@@ -1,6 +1,15 @@
-/**
+/*******************************************************************************
+ * Copyright (C) 2022, 1C-Soft LLC and others.
  *
- */
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial API and implementation
+ *******************************************************************************/
 package com.e1c.v8codestyle.form.check.itests;
 
 import static com._1c.g5.v8.dt.metadata.mdclass.MdClassPackage.Literals.CONFIGURATION;
