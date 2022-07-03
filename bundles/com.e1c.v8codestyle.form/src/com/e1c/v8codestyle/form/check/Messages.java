b9d720966f3dfce47b41a18a4bff63ea26116aa0
RedMammoth <64589646+RedMammoth@users.noreply.github.com>
2022-07-03 10:04:53 +0300

    #855 Проверка form-items-single-event-handler (#998)
    * Добавил проверку единичного использования обработчика событий

--- bundles/com.e1c.v8codestyle.form/src/com/e1c/v8codestyle/form/check/Messages.java
+++ bundles/com.e1c.v8codestyle.form/src/com/e1c/v8codestyle/form/check/Messages.java
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *     1C-Soft LLC - initial API and implementation
+ *     Manaev Konstantin - issue #855
  *******************************************************************************/
 package com.e1c.v8codestyle.form.check;
 
@@ -22,6 +23,10 @@ final class Messages
     extends NLS
 {
     private static final String BUNDLE_NAME = "com.e1c.v8codestyle.form.check.messages"; //$NON-NLS-1$
+    public static String FormItemsSingleEventHandlerCheck_description;
+    public static String FormItemsSingleEventHandlerCheck_itemName_dot_eventName;
+    public static String FormItemsSingleEventHandlerCheck_the_handler_is_already_assigned_to_event;
+    public static String FormItemsSingleEventHandlerCheck_title;
     public static String FormListRefUseAlwaysFlagDisabledCheck_description;
     public static String FormListRefUseAlwaysFlagDisabledCheck_title;
     public static String FormListRefUseAlwaysFlagDisabledCheck_UseAlways_flag_is_disabled_for_the_Ref_field;
--- bundles/com.e1c.v8codestyle.form/src/com/e1c/v8codestyle/internal/form/ExternalDependenciesModule.java
+++ bundles/com.e1c.v8codestyle.form/src/com/e1c/v8codestyle/internal/form/ExternalDependenciesModule.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2021, 1C-Soft LLC and others.
+ * Copyright (C) 2022, 1C-Soft LLC and others.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -9,12 +9,16 @@
  *
  * Contributors:
  *     1C-Soft LLC - initial API and implementation
+ *     Manaev Konstantin - issue #855
  *******************************************************************************/
 package com.e1c.v8codestyle.internal.form;
 
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.xtext.resource.IResourceServiceProvider;
 
 import com._1c.g5.v8.dt.core.platform.IV8ProjectManager;
+import com._1c.g5.v8.dt.form.service.FormItemInformationService;
 import com._1c.g5.v8.dt.form.service.datasourceinfo.IDataSourceInfoAssociationService;
 import com._1c.g5.wiring.AbstractServiceAwareModule;
 
@@ -39,6 +43,9 @@ public class ExternalDependenciesModule
     @Override
     protected void doConfigure()
     {
+        URI uri = URI.createURI("*.form"); //$NON-NLS-1$
+        final IResourceServiceProvider rsp = IResourceServiceProvider.Registry.INSTANCE.getResourceServiceProvider(uri);
+        bind(FormItemInformationService.class).toProvider(() -> rsp.get(FormItemInformationService.class));
         bind(IV8ProjectManager.class).toService();
         bind(IDataSourceInfoAssociationService.class).toService();
     }
