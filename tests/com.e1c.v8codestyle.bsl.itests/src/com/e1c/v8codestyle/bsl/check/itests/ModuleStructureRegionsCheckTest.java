d5dd6a169715ad94b33651dac4606db3c9f47621
Илюхин Артем Васильевич <ilua@1c.ru>
2022-07-07 10:59:24 +0300

    Добавлены тесты

--- tests/com.e1c.v8codestyle.bsl.itests/src/com/e1c/v8codestyle/bsl/check/itests/ModuleStructureRegionsCheckTest.java
+++ tests/com.e1c.v8codestyle.bsl.itests/src/com/e1c/v8codestyle/bsl/check/itests/ModuleStructureRegionsCheckTest.java
@@ -25,6 +25,7 @@ import com._1c.g5.v8.bm.core.IBmObject;
 import com._1c.g5.v8.dt.bsl.model.Method;
 import com._1c.g5.v8.dt.bsl.model.Module;
 import com._1c.g5.v8.dt.core.platform.IDtProject;
+import com._1c.g5.v8.dt.metadata.mdclass.Catalog;
 import com._1c.g5.v8.dt.metadata.mdclass.CommonModule;
 import com._1c.g5.v8.dt.validation.marker.Marker;
 import com.e1c.g5.v8.dt.testing.check.SingleProjectReadOnlyCheckTestBase;
@@ -46,6 +47,8 @@ public class ModuleStructureRegionsCheckTest
     private static final String FQN_COMMON_MODULE_AFTER_REGION = "CommonModule.CommonModuleAfterRegion";
     private static final String FQN_COMMON_MODULE_IN_REGION = "CommonModule.CommonModuleInRegion";
     private static final String FQN_COMMON_MODULE_EXPORT_IN_REGION = "CommonModule.CommonModuleExportInRegion";
+    private static final String FQN_CATALOG_MODULE_MANAGER_EVENT = "Catalog.CatalogInRegion";
+    private static final String FQN_CATALOG_MODULE_MANAGER_EVENT_OUT_REGION = "Catalog.CatalogOutOfRegion";
 
     @Override
     protected String getTestConfigurationName()
@@ -139,6 +142,50 @@ public class ModuleStructureRegionsCheckTest
 
         Marker marker = getFirstMarker(CHECK_ID, method, getProject());
         assertNull(marker);
+    }
+
+    @Test
+    public void testEventModuleManagerInRegion() throws Exception
+    {
+        IDtProject dtProject = dtProjectManager.getDtProject(PROJECT_NAME);
+        assertNotNull(dtProject);
+
+        IBmObject mdObject = getTopObjectByFqn(FQN_CATALOG_MODULE_MANAGER_EVENT, dtProject);
+        assertTrue(mdObject instanceof Catalog);
+
+        Module module = ((Catalog)mdObject).getManagerModule();
+        assertNotNull(module);
+
+        List<Method> methods = module.allMethods();
+        assertFalse(methods.isEmpty());
+
+        Method method = methods.get(0);
+        assertNotNull(method);
+
+        Marker marker = getFirstMarker(CHECK_ID, method, getProject());
+        assertNull(marker);
+    }
+
+    @Test
+    public void testEventModuleManagerOutOfRegion() throws Exception
+    {
+        IDtProject dtProject = dtProjectManager.getDtProject(PROJECT_NAME);
+        assertNotNull(dtProject);
+
+        IBmObject mdObject = getTopObjectByFqn(FQN_CATALOG_MODULE_MANAGER_EVENT_OUT_REGION, dtProject);
+        assertTrue(mdObject instanceof Catalog);
+
+        Module module = ((Catalog)mdObject).getManagerModule();
+        assertNotNull(module);
+
+        List<Method> methods = module.allMethods();
+        assertFalse(methods.isEmpty());
+
+        Method method = methods.get(0);
+        assertNotNull(method);
+
+        Marker marker = getFirstMarker(CHECK_ID, method, getProject());
+        assertNotNull(marker);
     }
 
 }
