28c04f365d506e767acabdefa1c003b1d8b291bc
Dmitriy Marmyshev <dmar@1c.ru>
2022-08-03 18:56:33 +0300

    Проверка: doc-comment-parameter-section не находит ошибку #1087

---
+++ tests/com.e1c.v8codestyle.bsl.itests/src/com/e1c/v8codestyle/bsl/comment/check/itests/RedundantParametersSectionCheckTest.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (C) 2021, 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial API and implementation
+ *******************************************************************************/
+package com.e1c.v8codestyle.bsl.comment.check.itests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com._1c.g5.v8.dt.validation.marker.IExtraInfoKeys;
+import com._1c.g5.v8.dt.validation.marker.Marker;
+import com.e1c.v8codestyle.bsl.check.itests.AbstractSingleModuleTestBase;
+import com.e1c.v8codestyle.bsl.comment.check.RedundantParametersSectionCheck;
+
+/**
+ * Tests for {@link RedundantParametersSectionCheck} check.
+ *
+ * @author Dmitriy Marmyshev
+ */
+public class RedundantParametersSectionCheckTest
+    extends AbstractSingleModuleTestBase
+{
+
+    public RedundantParametersSectionCheckTest()
+    {
+        super(RedundantParametersSectionCheck.class);
+    }
+
+    /**
+     * Test the documentation comment should not have parameter section for method without parameters
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void testAllMethodParametersShouldCheck() throws Exception
+    {
+        updateModule(FOLDER_RESOURCE + "doc-comment-redundant-parameter-section.bsl");
+
+        List<Marker> markers = getModuleMarkers();
+        assertEquals(1, markers.size());
+        Marker marker = markers.get(0);
+        assertEquals("2", marker.getExtraInfo().get(IExtraInfoKeys.TEXT_EXTRA_INFO_LINE_KEY));
+    }
+}
