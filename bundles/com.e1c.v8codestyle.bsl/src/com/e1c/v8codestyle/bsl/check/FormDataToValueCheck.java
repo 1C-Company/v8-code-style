244f708dc43eea3b006155a51d0e1712839584d2
Artem Iliukhin <iartemv@gmail.com>
2022-11-02 15:29:48 +0300

    Добавлены тесты

--- bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/check/FormDataToValueCheck.java
+++ bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/check/FormDataToValueCheck.java
@@ -1,6 +1,15 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (C) 2022, 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial API and implementation
+ *******************************************************************************/
 package com.e1c.v8codestyle.bsl.check;
 
 import static com._1c.g5.v8.dt.bsl.model.BslPackage.Literals.FEATURE_ACCESS;
---
+++ tests/com.e1c.v8codestyle.bsl.itests/src/com/e1c/v8codestyle/bsl/check/itests/FormDataToValueCheckTest.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (C) 2022, 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial API and implementation
+ *******************************************************************************/
+package com.e1c.v8codestyle.bsl.check.itests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com._1c.g5.v8.bm.core.IBmObject;
+import com._1c.g5.v8.dt.bsl.model.Module;
+import com._1c.g5.v8.dt.metadata.mdclass.AbstractForm;
+import com._1c.g5.v8.dt.validation.marker.IExtraInfoKeys;
+import com._1c.g5.v8.dt.validation.marker.Marker;
+import com.e1c.v8codestyle.bsl.check.FormDataToValueCheck;
+
+/**
+ * The test for {@link FormDataToValueCheck} check.
+ * 
+ * @author Artem Iliukhin
+ */
+public class FormDataToValueCheckTest
+    extends AbstractSingleModuleTestBase
+{
+    private static final String PROJECT_NAME = "CommonForm";
+
+    private static final String FQN = "CommonForm.Form.Form";
+
+    private static final String COMMON_FORM_FILE_NAME = "/src/CommonForms/Form/Module.bsl";
+
+    public FormDataToValueCheckTest()
+    {
+        super(FormDataToValueCheck.class);
+    }
+
+    @Override
+    protected String getTestConfigurationName()
+    {
+        return PROJECT_NAME;
+    }
+
+    @Override
+    protected String getModuleFileName()
+    {
+        return COMMON_FORM_FILE_NAME;
+    }
+
+    @Override
+    protected Module getModule()
+    {
+        IBmObject mdObject = getTopObjectByFqn(FQN, getProject());
+        assertTrue(mdObject instanceof AbstractForm);
+        Module module = ((AbstractForm)mdObject).getModule();
+        assertNotNull(module);
+
+        return module;
+    }
+
+    @Test
+    public void testFormModuleHasPragma() throws Exception
+    {
+        updateModule(FOLDER_RESOURCE + "using-form-data-to-value.bsl");
+
+        Marker marker = getModuleFirstMarker();
+        assertEquals("2", marker.getExtraInfo().get(IExtraInfoKeys.TEXT_EXTRA_INFO_LINE_KEY));
+    }
+}
