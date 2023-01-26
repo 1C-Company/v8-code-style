4b513d426c33afc8ffdfcf111cb165f248a39097
Илюхин Артем Васильевич <ilua@1c.ru>
2023-01-26 10:36:06 +0300

    Исправление замечаний cr

--- bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/check/CodeAfterAsyncCallCheck.java
+++ bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/check/CodeAfterAsyncCallCheck.java
@@ -14,53 +14,40 @@ package com.e1c.v8codestyle.bsl.check;
 
 import static com._1c.g5.v8.dt.bsl.model.BslPackage.Literals.INVOCATION;
 
-import java.util.Iterator;
-import java.util.List;
-
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.naming.IQualifiedNameConverter;
 
 import com._1c.g5.v8.dt.bsl.common.IBslPreferences;
-import com._1c.g5.v8.dt.bsl.model.BslPackage;
-import com._1c.g5.v8.dt.bsl.model.Conditional;
 import com._1c.g5.v8.dt.bsl.model.EmptyStatement;
 import com._1c.g5.v8.dt.bsl.model.FeatureAccess;
-import com._1c.g5.v8.dt.bsl.model.IfStatement;
 import com._1c.g5.v8.dt.bsl.model.Invocation;
-import com._1c.g5.v8.dt.bsl.model.LoopStatement;
-import com._1c.g5.v8.dt.bsl.model.PreprocessorItemStatements;
 import com._1c.g5.v8.dt.bsl.model.ReturnStatement;
 import com._1c.g5.v8.dt.bsl.model.Statement;
 import com._1c.g5.v8.dt.bsl.model.StaticFeatureAccess;
-import com._1c.g5.v8.dt.bsl.model.TryExceptStatement;
 import com._1c.g5.v8.dt.core.platform.IResourceLookup;
 import com.e1c.g5.v8.dt.check.CheckComplexity;
 import com.e1c.g5.v8.dt.check.ICheckParameters;
 import com.e1c.g5.v8.dt.check.settings.IssueSeverity;
 import com.e1c.g5.v8.dt.check.settings.IssueType;
-import com.e1c.v8codestyle.bsl.strict.check.AbstractTypeCheck;
 import com.e1c.v8codestyle.check.CommonSenseCheckExtension;
 import com.e1c.v8codestyle.internal.bsl.BslPlugin;
-import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 
 /**
- * Checks that the asynchronous method is not followed by lines of code, 
- * since in this case the specified lines of code are executed immediately, 
+ * Checks that the asynchronous method is not followed by lines of code,
+ * since in this case the specified lines of code are executed immediately,
  * without waiting for the asynchronous method to execute.
- * 
+ *
  * @author Artem Iliukhin
  */
 public final class CodeAfterAsyncCallCheck
-    extends AbstractTypeCheck
+    extends AbstractTransactionCheck
 {
 
     private static final String CHECK_ID = "code-after-async-call"; //$NON-NLS-1$
 
     // @formatter:off
-    private static final String[] ASYNCHRONOUS_METHODS = { 
+    private static final String[] ASYNCHRONOUS_METHODS = {
         "ShowMessageBox", "ПоказатьПредупреждение", //$NON-NLS-1$ //$NON-NLS-2$
         "ShowInputDate", "ПоказатьВводДаты", //$NON-NLS-1$ //$NON-NLS-2$
         "ShowInputValue", "ПоказатьВводЗначения", //$NON-NLS-1$ //$NON-NLS-2$
@@ -92,7 +79,7 @@ public final class CodeAfterAsyncCallCheck
     public CodeAfterAsyncCallCheck(IResourceLookup resourceLookup, IBslPreferences bslPreferences,
         IQualifiedNameConverter qualifiedNameConverter)
     {
-        super(resourceLookup, bslPreferences, qualifiedNameConverter);
+        super();
     }
 
     @Override
@@ -109,6 +96,7 @@ public final class CodeAfterAsyncCallCheck
             .complexity(CheckComplexity.NORMAL)
             .severity(IssueSeverity.MAJOR)
             .issueType(IssueType.WARNING)
+            .disable()
             .extension(new CommonSenseCheckExtension(getCheckId(), BslPlugin.PLUGIN_ID))
             .module()
             .checkedObjectType(INVOCATION);
@@ -127,10 +115,10 @@ public final class CodeAfterAsyncCallCheck
                 return;
             }
 
-            String nameFeature = featureAccess.getName();
+            String featureName = featureAccess.getName();
             for (int i = 0; i < ASYNCHRONOUS_METHODS.length; i++)
             {
-                if (ASYNCHRONOUS_METHODS[i].equalsIgnoreCase(nameFeature))
+                if (ASYNCHRONOUS_METHODS[i].equalsIgnoreCase(featureName))
                 {
                     Statement statement = getStatementFromInvoc(inv);
                     if (statement != null)
@@ -145,73 +133,5 @@ public final class CodeAfterAsyncCallCheck
                 }
             }
         }
-    }
-
-    private Statement getStatementFromInvoc(Invocation invocation)
-    {
-        EObject container = invocation.eContainer();
-        while (!(container instanceof Statement))
-        {
-            container = container.eContainer();
-        }
-        return container instanceof Statement ? (Statement)container : null;
-    }
-
-    private Statement getNextStatement(Statement statement)
-    {
-        Iterator<EObject> it = EcoreUtil2.getAllContainers(statement).iterator();
-        while (it.hasNext())
-        {
-            EObject container = it.next();
-            List<Statement> st = null;
-            if (container instanceof LoopStatement)
-            {
-                st = ((LoopStatement)container).getStatements();
-            }
-            else if (container instanceof Conditional)
-            {
-                st = ((Conditional)container).getStatements();
-            }
-            else if (container instanceof IfStatement)
-            {
-                st = ((IfStatement)container).getElseStatements();
-            }
-            else if (container instanceof TryExceptStatement)
-            {
-                st = getStatementsFromContainer((TryExceptStatement)container);
-            }
-            else if (container instanceof PreprocessorItemStatements)
-            {
-                st = ((PreprocessorItemStatements)container).getStatements();
-            }
-            else
-            {
-                st = getStatementsFromContainer(container);
-            }
-            if (st != null)
-            {
-                int index = st.indexOf(statement);
-                if (index != -1 && index + 1 < st.size())
-                {
-                    return st.get(index + 1);
-                }
-            }
-        }
-        return null;
-    }
-
-    private List<Statement> getStatementsFromContainer(TryExceptStatement container)
-    {
-        List<Statement> res = Lists.newArrayList();
-        res.addAll(container.getTryStatements());
-        res.addAll(container.getExceptStatements());
-        return res;
-    }
-
-    @SuppressWarnings("unchecked")
-    private List<Statement> getStatementsFromContainer(EObject container)
-    {
-        Object obj = container.eGet(BslPackage.Literals.BLOCK__STATEMENTS);
-        return obj instanceof List ? (List<Statement>)obj : null;
     }
 }
