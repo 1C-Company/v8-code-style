b7a9091788b97edda8f28b3d0ade32949a619ebc
Vadim Gocnharov <goncharov.vdm@yandex.ru>
2023-03-03 06:14:04 +0300

    #262 Исправлены замечания оформления кода
    Исправлены замечания оформления кода при сборке pr.

--- bundles/com.e1c.v8codestyle.form/src/com/e1c/v8codestyle/form/check/DynamicListConditionalAppearanceUseCheck.java
+++ bundles/com.e1c.v8codestyle.form/src/com/e1c/v8codestyle/form/check/DynamicListConditionalAppearanceUseCheck.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (C) 2023, 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial API and implementation
+ *******************************************************************************/
+
 package com.e1c.v8codestyle.form.check;
 
 import static com._1c.g5.v8.dt.form.model.FormPackage.Literals.DYNAMIC_LIST_EXT_INFO;
