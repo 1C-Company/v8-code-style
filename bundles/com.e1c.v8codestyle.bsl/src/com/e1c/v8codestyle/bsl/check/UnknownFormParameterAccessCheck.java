78778ac48a8c92df7a9bb52436d390f3fb8ea91f
Vadim Goncharov <goncharov.vad@gmail.com>
2023-03-14 13:34:31 +0300

    #1119 Переделаны шапки с копирайтом
    В классах проверки и тестирования переделаны шапки с копирайтом.

--- bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/check/UnknownFormParameterAccessCheck.java
+++ bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/check/UnknownFormParameterAccessCheck.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2022, 1C-Soft LLC and others.
+ * Copyright (C) 2023, 1C-Soft LLC and others.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
--- tests/com.e1c.v8codestyle.bsl.itests/src/com/e1c/v8codestyle/bsl/check/itests/UnknownFormParameterAccessCheckTest.java
+++ tests/com.e1c.v8codestyle.bsl.itests/src/com/e1c/v8codestyle/bsl/check/itests/UnknownFormParameterAccessCheckTest.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (C) 2023, 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial API and implementation
+ *******************************************************************************/
+
 package com.e1c.v8codestyle.bsl.check.itests;
 
 import static org.junit.Assert.assertEquals;
