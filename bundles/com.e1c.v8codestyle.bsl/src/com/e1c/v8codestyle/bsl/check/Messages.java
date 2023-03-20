bb151bf321e453d7ce3492a234e016b0df9dcb57
Vadim Goncharov <goncharov.vad@gmail.com>
2023-03-20 13:45:09 +0300

    #385 Проверка имени переменной (#1253)
    * #385 Реализация стандарта 454 - проверка имени переменной

--- bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/check/Messages.java
+++ bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/check/Messages.java
@@ -434,6 +434,14 @@ final class Messages
 
     public static String LockOutOfTry_Method_lock_out_of_try;
 
+    public static String VariableNameInvalidCheck_description;
+    public static String VariableNameInvalidCheck_message_variable_length_is_less_than;
+    public static String VariableNameInvalidCheck_param_MIN_NAME_LENGTH_PARAM_title;
+    public static String VariableNameInvalidCheck_title;
+    public static String VariableNameInvalidCheck_variable_name_is_invalid;
+    public static String VariableNameInvalidCheck_variable_name_must_start_with_a_capital_letter;
+    public static String VariableNameInvalidCheck_variable_name_starts_with_an_underline;
+
     static
     {
         // initialize resource bundle
