b5eb373b1952c3e414a975d6f4f4fb235e205c58
Almaz Nasibullin <AlmazNasibullin@users.noreply.github.com>
2023-03-25 18:20:55 +0300

    Сделана адаптация по задаче переноса настроек сортировки в кору 1C:EDT
    - В свойствах проекта в секции "Авто сортировка" более недоступна
    настройка направления и порядка сортировки.
    - В свойствах проекта в секции "Авто сортировка" добавлена возможность
    переноса заданных пользователем настроек направления и порядка
    сортировки в секцию "Сортировка объектов метаданных" и их очистки.

---
+++ bundles/com.e1c.v8codestyle.autosort.ui/src/com/e1c/v8codestyle/internal/autosort/ui/Messages.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (C) 2023, 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial API and implementation
+ *******************************************************************************/
+package com.e1c.v8codestyle.internal.autosort.ui;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * The localized messages.
+ *
+ * @author Almaz Nasibullin
+ */
+class Messages
+    extends NLS
+{
+    private static final String BUNDLE_NAME = "com.e1c.v8codestyle.internal.autosort.ui.messages"; //$NON-NLS-1$
+
+    public static String MdSortPreferenceChangeListener_Job_name;
+    public static String MdSortPreferenceChangeListener_Sort_question;
+    public static String MdSortPreferenceChangeListener_Sort_question_title;
+
+    static
+    {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+    private Messages()
+    {
+    }
+}
