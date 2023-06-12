1f0aac659103e39de0df1e7a9107559bc1c2f36f
Vadim Goncharov <goncharov.vad@gmail.com>
2023-06-12 05:20:50 +0300

    #1329 Исправлено формирование

--- bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/check/ManagerModuleNamedSelfReferenceCheck.java
+++ bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/check/ManagerModuleNamedSelfReferenceCheck.java
@@ -61,32 +61,33 @@ public class ManagerModuleNamedSelfReferenceCheck
     private final IQualifiedNameConverter qualifiedNameConverter;
 
     private final ITopObjectFqnGenerator topObjectFqnGenerator;
-    
+
     private static final Map<EReference, EClass> MANAGERS_FOR_MDOBJECT = new ImmutableMap.Builder<EReference, EClass>()
-         .put(MdClassPackage.Literals.CONFIGURATION__CATALOGS, MdClassPackage.Literals.CATALOG)
-         .put(MdClassPackage.Literals.CONFIGURATION__DOCUMENTS, MdClassPackage.Literals.DOCUMENT)
-         .put(MdClassPackage.Literals.CONFIGURATION__DOCUMENT_JOURNALS, MdClassPackage.Literals.DOCUMENT_JOURNAL)
-         .put(MdClassPackage.Literals.CONFIGURATION__ENUMS, MdClassPackage.Literals.ENUM)
-         .put(MdClassPackage.Literals.CONFIGURATION__REPORTS, MdClassPackage.Literals.REPORT)
-         .put(MdClassPackage.Literals.CONFIGURATION__DATA_PROCESSORS, MdClassPackage.Literals.DATA_PROCESSOR)
-         .put(MdClassPackage.Literals.CONFIGURATION__CHARTS_OF_CHARACTERISTIC_TYPES, MdClassPackage.Literals.CHART_OF_CHARACTERISTIC_TYPES)
-         .put(MdClassPackage.Literals.CONFIGURATION__CHARTS_OF_ACCOUNTS, MdClassPackage.Literals.CHART_OF_ACCOUNTS)
-         .put(MdClassPackage.Literals.CONFIGURATION__CHARTS_OF_CALCULATION_TYPES, MdClassPackage.Literals.CHART_OF_CALCULATION_TYPES)
-         .put(MdClassPackage.Literals.CONFIGURATION__INFORMATION_REGISTERS, MdClassPackage.Literals.INFORMATION_REGISTER)
-         .put(MdClassPackage.Literals.CONFIGURATION__ACCUMULATION_REGISTERS, MdClassPackage.Literals.ACCUMULATION_REGISTER)
-         .put(MdClassPackage.Literals.CONFIGURATION__ACCOUNTING_REGISTERS, MdClassPackage.Literals.ACCOUNTING_REGISTER)
-         .put(MdClassPackage.Literals.CONFIGURATION__CALCULATION_REGISTERS, MdClassPackage.Literals.CALCULATION_REGISTER)
-         .put(MdClassPackage.Literals.CONFIGURATION__BUSINESS_PROCESSES, MdClassPackage.Literals.BUSINESS_PROCESS)
-         .put(MdClassPackage.Literals.CONFIGURATION__TASKS, MdClassPackage.Literals.TASK)
-         .put(MdClassPackage.Literals.CONFIGURATION__EXCHANGE_PLANS, MdClassPackage.Literals.EXCHANGE_PLAN)
-         .put(MdClassPackage.Literals.CONFIGURATION__EXTERNAL_DATA_SOURCES, MdClassPackage.Literals.EXTERNAL_DATA_SOURCE)
-         .build();
-        
-    
+        .put(MdClassPackage.Literals.CONFIGURATION__CATALOGS, MdClassPackage.Literals.CATALOG)
+        .put(MdClassPackage.Literals.CONFIGURATION__DOCUMENTS, MdClassPackage.Literals.DOCUMENT)
+        .put(MdClassPackage.Literals.CONFIGURATION__DOCUMENT_JOURNALS, MdClassPackage.Literals.DOCUMENT_JOURNAL)
+        .put(MdClassPackage.Literals.CONFIGURATION__ENUMS, MdClassPackage.Literals.ENUM)
+        .put(MdClassPackage.Literals.CONFIGURATION__REPORTS, MdClassPackage.Literals.REPORT)
+        .put(MdClassPackage.Literals.CONFIGURATION__DATA_PROCESSORS, MdClassPackage.Literals.DATA_PROCESSOR)
+        .put(MdClassPackage.Literals.CONFIGURATION__CHARTS_OF_CHARACTERISTIC_TYPES,
+            MdClassPackage.Literals.CHART_OF_CHARACTERISTIC_TYPES)
+        .put(MdClassPackage.Literals.CONFIGURATION__CHARTS_OF_ACCOUNTS, MdClassPackage.Literals.CHART_OF_ACCOUNTS)
+        .put(MdClassPackage.Literals.CONFIGURATION__CHARTS_OF_CALCULATION_TYPES,
+            MdClassPackage.Literals.CHART_OF_CALCULATION_TYPES)
+        .put(MdClassPackage.Literals.CONFIGURATION__INFORMATION_REGISTERS, MdClassPackage.Literals.INFORMATION_REGISTER)
+        .put(MdClassPackage.Literals.CONFIGURATION__ACCUMULATION_REGISTERS,
+            MdClassPackage.Literals.ACCUMULATION_REGISTER)
+        .put(MdClassPackage.Literals.CONFIGURATION__ACCOUNTING_REGISTERS, MdClassPackage.Literals.ACCOUNTING_REGISTER)
+        .put(MdClassPackage.Literals.CONFIGURATION__CALCULATION_REGISTERS, MdClassPackage.Literals.CALCULATION_REGISTER)
+        .put(MdClassPackage.Literals.CONFIGURATION__BUSINESS_PROCESSES, MdClassPackage.Literals.BUSINESS_PROCESS)
+        .put(MdClassPackage.Literals.CONFIGURATION__TASKS, MdClassPackage.Literals.TASK)
+        .put(MdClassPackage.Literals.CONFIGURATION__EXCHANGE_PLANS, MdClassPackage.Literals.EXCHANGE_PLAN)
+        .put(MdClassPackage.Literals.CONFIGURATION__EXTERNAL_DATA_SOURCES, MdClassPackage.Literals.EXTERNAL_DATA_SOURCE)
+        .build();
+
     @Inject
     public ManagerModuleNamedSelfReferenceCheck(IScopeProvider scopeProvider,
-        IQualifiedNameConverter qualifiedNameConverter,
-        ITopObjectFqnGenerator topObjectFqnGenerator)
+        IQualifiedNameConverter qualifiedNameConverter, ITopObjectFqnGenerator topObjectFqnGenerator)
     {
         this.scopeProvider = scopeProvider;
         this.qualifiedNameConverter = qualifiedNameConverter;
@@ -134,26 +135,26 @@ public class ManagerModuleNamedSelfReferenceCheck
         }
 
         StaticFeatureAccess managerType = (StaticFeatureAccess)managerTypeExpression;
-        
-        EReference eRef = getConfigurationMdObjectRef(managerType); 
-        if (monitor.isCanceled() ||  eRef == null)
+
+        EReference eRef = getConfigurationMdObjectRef(managerType);
+        if (monitor.isCanceled() || eRef == null)
         {
             return;
         }
-        
+
         IEObjectDescription objectDesc = getObjectFromScope(source, eRef);
         if (monitor.isCanceled() || objectDesc == null)
         {
             return;
         }
-        
+
         EObject eObject = objectDesc.getEObjectOrProxy();
         if (!monitor.isCanceled() && eObject.equals(module.getOwner()))
         {
             resultAceptor.addIssue(Messages.ManagerModuleNamedSelfReferenceCheck_issue, source);
         }
     }
-    
+
     private EReference getConfigurationMdObjectRef(StaticFeatureAccess managerType)
     {
         EReference result = null;
@@ -165,12 +166,13 @@ public class ManagerModuleNamedSelfReferenceCheck
         }
         return result;
     }
-    
+
     private IEObjectDescription getObjectFromScope(DynamicFeatureAccess source, EReference reference)
     {
         IScope scope = scopeProvider.getScope(source, reference);
-        String fqn = topObjectFqnGenerator.generateStandaloneObjectFqn(MANAGERS_FOR_MDOBJECT.get(reference), source.getName());
+        String fqn =
+            topObjectFqnGenerator.generateStandaloneObjectFqn(MANAGERS_FOR_MDOBJECT.get(reference), source.getName());
         return scope.getSingleElement(qualifiedNameConverter.toQualifiedName(fqn));
     }
-    
+
 }
